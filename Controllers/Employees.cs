46f006eeca767c09600818dc6ff67b68ddce2d07
CMotillon <cedric@DESKTOP-V4CDAOU.localdomain>
2021-04-27 15:41:27 -0400

    Added endpoint for mobile app

---
+++ Controllers/Employees.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rocket_Elevators_Rest_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Rocket_Elevators_Rest_API.Data;
+using Microsoft.AspNetCore.Cors;
+
+using Pomelo.EntityFrameworkCore.MySql;
+
+
+
+namespace Rocket_Elevators_Rest_API.Models.Controllers
+{   [ApiController]
+    [Route("api/[controller]")]
+   
+    public class EmployeesController : ControllerBase
+    {
+        private readonly rocketelevators_developmentContext _context;
+
+        public EmployeesController(rocketelevators_developmentContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employees>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        [HttpGet("Check/{email}")]
+        public bool Employees(string email)
+        {
+            var user = _context.Users.Any(u => u.Email == email);
+            return _context.Employees.Any(e => e.Id == user.id);
+        }
+    }
+}
